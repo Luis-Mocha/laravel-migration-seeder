d538e7bd620e957b5e7738d7930f040e17a0f420
Luis-Mocha <luismocha97@gmail.com>
2023-06-12 18:05:52 +0200

    seeding table

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -8,6 +8,9 @@ use Illuminate\Database\Seeder;
 // implemento il modello
 use App\Models\Train;
 
+// implemento faker
+use Faker\Generator as Faker;
+
 
 class TrainsTableSeeder extends Seeder
 {
@@ -16,8 +19,23 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 10; $i++) {
+
+            $newTrain = new Train();
+            $newTrain->nome_azienda = $faker->company();
+            $newTrain->stazione_partenza = $faker->city();
+            $newTrain->stazione_arrivo = $faker->city();
+            $newTrain->orario_partenza = $faker->time();
+            $newTrain->orario_arrivo = $faker->time();
+            $newTrain->codice_treno = $faker->unique()->numberBetween(10, 99);
+            $newTrain->numero_carrozze = $faker->numberBetween(5, 10);
+            $newTrain->in_orario = true;
+            $newTrain->cancellato = false;
+
+            $newTrain->save();
+
+        }
     }
 }
