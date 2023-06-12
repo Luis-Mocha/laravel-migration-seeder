106565f03c2b988eb346937cc0bba8a4f1f86721
Luis-Mocha <luismocha97@gmail.com>
2023-06-12 15:37:11 +0200

    nuova pagina iniziale

---
+++ resources/views/pages/trains.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app')
+
+@section ('pagetitle-content')
+Esercizio migration/seeder | Treni
+@endsection
+
+@section ('main-content')
+
+    <div>
+        <h1 class="text-center py-2">Pagina treni</h1>
+    </div>
+
+@endsection
