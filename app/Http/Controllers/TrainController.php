0b0d6be4dacf89b020c9790ead2df1f664397045
Luis-Mocha <luismocha97@gmail.com>
2023-06-12 18:11:54 +0200

    chiamo i dati dal databse

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+//implememtno l'uso del modello train
+use App\Models\Train;
+
 class TrainController extends Controller
 {
     public function getTrain() {
-        return view('pages.trains');
+
+        $trains = Train::All();
+
+        return view('pages.trains', compact('trains'));
     }
     
 }
