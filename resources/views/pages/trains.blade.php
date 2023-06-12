5ab204b1884887c0923abb37448b8f22809ed40c
Luis-Mocha <luismocha97@gmail.com>
2023-06-12 18:33:05 +0200

    stampo i biglietti / fine esercizio

--- resources/views/pages/trains.blade.php
+++ resources/views/pages/trains.blade.php
@@ -11,9 +11,31 @@ Esercizio migration/seeder | Treni
     </div>
 
     <div class="container">
-@php
-    dd($trains);
-@endphp
+    @foreach($trains as $elem)
+
+        <div class="ticket d-flex  border mb-3 rounded py-3 px-2">
+
+            <div class="d-flex flex-column w-25 border-end px-1">
+                <span class="fst-italic fw-light">Compagnia: {{$elem['nome_azienda']}}</span>
+                <span>Codice Treno: {{$elem['codice_treno']}}</span>
+                <span>Numero Carrozze: {{$elem['numero_carrozze']}}</span>
+            </div>
+
+            <div class="text-center w-50">
+                <div>Stazione di partenza: <span class="fs-5 fw-semibold">{{$elem['stazione_partenza']}}</span> </div>
+
+                <div >Orario partenza: <span class="fs-5 fw-semibold">{{$elem['orario_partenza']}}</span> </div>
+            </div>
+
+            <div class="text-center w-25 border-start px-1">
+                <div>Stazione di arrivo: <span class="fw-semibold">{{$elem['stazione_partenza']}}</span> </div>
+
+                <div >Orario arrivo: <span class="fw-semibold">{{$elem['orario_partenza']}}</span> </div>
+            </div>
+
+        </div>
+
+    @endforeach
 
     </div>
 
