911c1b89b674f47e96ae5befc31d422d906bd7f3
Luis-Mocha <luismocha97@gmail.com>
2023-06-12 17:32:59 +0200

    creazione seeder

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+// implemento il modello
+use App\Models\Train;
+
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
